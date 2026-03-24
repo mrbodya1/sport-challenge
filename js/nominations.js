4f4d4f0f4fc4365dd3b0b2e072f1849ded23b34e
mrbodya1 <mr.bodya1@gmail.com>
2026-03-24 23:15:39 +0500

    Delete js directory

--- js/nominations.js
+++
@@ -1,6 +0,0 @@
-// Номинации и ачивки
-async function loadNominations(genderFilter) { /* ... */ }
-async function loadNominationTable(containerId, tableName, genderFilter, valueField, unit) { /* ... */ }
-
-async function renderAchievementsPage() { /* ... */ }
-async function renderNominationsPage() { /* ... */ }
