870846bea616f6c3d24d1d7929dacb7aa0bac240
mrbodya1 <mr.bodya1@gmail.com>
2026-03-24 22:13:31 +0500

    Update nominations.js

--- js/nominations.js
+++ js/nominations.js
@@ -1,1 +1,6 @@
+// Номинации и ачивки
+async function loadNominations(genderFilter) { /* ... */ }
+async function loadNominationTable(containerId, tableName, genderFilter, valueField, unit) { /* ... */ }
 
+async function renderAchievementsPage() { /* ... */ }
+async function renderNominationsPage() { /* ... */ }
