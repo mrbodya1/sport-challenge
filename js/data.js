e059f1b74abe6eae4ff4ffff4b2d08d915b88a7f
mrbodya1 <mr.bodya1@gmail.com>
2026-03-24 22:13:21 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,10 @@
-
+// Загрузка данных
+async function loadProfile() { /* ... */ }
+async function loadStats() { /* ... */ }
+async function loadPlaces(profile) { /* ... */ }
+async function loadWeeklyChart() { /* ... */ }
+async function loadRecentTrainings() { /* ... */ }
+async function loadTopLeaderboard() { /* ... */ }
+async function loadTrainings(dateFilter = '') { /* ... */ }
+async function loadLeaderboardFull(genderFilter, ageFilter, sortBy) { /* ... */ }
+async function loadFeed(genderFilter, ageFilter, dateFilter, loadMore) { /* ... */ }
