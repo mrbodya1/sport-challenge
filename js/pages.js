c0650ff7972969058dfb11af6369385e4c632a8e
mrbodya1 <mr.bodya1@gmail.com>
2026-03-24 22:13:42 +0500

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,1 +1,15 @@
+// Отрисовка страниц
+function renderAuth() { /* ... */ }
+async function renderHome() { /* ... */ }
+async function renderAddPage() { /* ... */ }
+async function renderTrainingsPage() { /* ... */ }
+async function renderLeaderboardPage() { /* ... */ }
+async function renderFeedPage() { /* ... */ }
+async function renderAbilitiesPage() { /* ... */ }
+async function renderSettingsPage() { /* ... */ }
+async function renderPointsPage() { /* ... */ }
+async function renderUserProfile() { /* ... */ }
 
+async function loadHomeData() { /* ... */ }
+async function loadSettings() { /* ... */ }
+async function saveSettings() { /* ... */ }
